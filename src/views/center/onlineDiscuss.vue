aee747d4504a212a85addd267b9bbf3830e5ead6
Arron Zhu <1274294199@qq.com>
2019-02-19 17:23:46 +0800

    个人中心页面基础

--- src/views/center/onlineDiscuss.vue
+++ src/views/center/onlineDiscuss.vue
@@ -1,12 +1,12 @@
 <template>
-  <div></div>
-</template>
+  <div>
 
+  </div>
+</template>
 <script>
 export default {
-  name: 'MyInfo'
+  name: 'online-discuss'
 }
 </script>
-
 <style scoped>
 </style>
